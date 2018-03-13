91b411d24e873a5936b8049fd414e042ee4ac1b9
Okamotom <ec2-user@ip-172-31-2-152.us-east-2.compute.internal>
2018-03-13 08:37:18 +0000

    ranking_addhave

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -3,4 +3,8 @@ class RankingsController < ApplicationController
     @ranking_counts = Want.ranking #[3=>10, 5=>6, 2=>5, 8=>3, ...] のように [item_id => Want の数]が入る
     @items = Item.find(@ranking_counts.keys) #@ranking_counts.keys でハッシュから item_id だけ取り出す。findした@items はランキングの順番通りの並び順
   end
+  def have
+    @ranking_counts = Have.ranking #[3=>10, 5=>6, 2=>5, 8=>3, ...] のように [item_id => Have の数]が入る
+    @items = Item.find(@ranking_counts.keys) #@ranking_counts.keys でハッシュから item_id だけ取り出す。findした@items はランキングの順番通りの並び順
+  end
 end
